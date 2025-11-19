3d1477094636f82eed8dea070972c0362857120e
aangelainis <angelinahenriquechaves@gmail.com>
2025-11-19 16:43:47 -0300

    fix directories

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6456889934d588f35f24d74e2513ec741297da76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea6ffc4703154303c1e554b37ce59a35044576b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
